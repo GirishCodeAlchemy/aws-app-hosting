49ab5de0d084d7d5020cb42ca170e9815b1e3605
girish.v <girish.v@eagleview.com>
2023-09-02 20:49:36 +0530

    Updated msk configuration

--- msk.tf
+++ msk.tf
@@ -3,18 +3,20 @@ resource "aws_msk_cluster" "demo-msk" {
   kafka_version          = "2.8.1"
   number_of_broker_nodes = 2
   broker_node_group_info {
-    instance_type  = "kafka.t3.small"                        # Choose an appropriate instance type
-    client_subnets = distinct(data.aws_subnets.client_subnets.ids) # Specify your VPC subnets
+    instance_type   = "kafka.t3.small" # Choose an appropriate instance type
+    ebs_volume_size = 50
+    client_subnets  = distinct(data.aws_subnets.client_subnets.ids) # Specify your VPC subnets
   }
 
   encryption_info {
     encryption_in_transit {
       client_broker = "PLAINTEXT"
-      in_cluster    = "PLAINTEXT"
+      in_cluster    = false
     }
   }
   configuration_info {
-    arn = aws_msk_configuration.demo-msk-configuration.arn
+    arn      = aws_msk_configuration.demo-msk-configuration.arn
+    revision = aws_msk_configuration.demo-msk-configuration.revision
   }
 
   tags = {
